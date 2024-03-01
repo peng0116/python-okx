1449fbc17498ca93d5a80d39aaa209a0234e46d0
SHAREit <shareit@Mac-VWLV6V2TXL.local>
2024-03-01 19:45:25 +0800

    auto

--- service/CopyTradingService.py
+++ service/CopyTradingService.py
@@ -2,6 +2,7 @@ from okx import CopyTrading
 from okx import Account
 from APIKEY import *
 import json
+import time
 
 if __name__ == '__main__':
     # flag是实盘与模拟盘的切换参数 flag is the key parameter which can help you to change between demo and real trading.
@@ -16,9 +17,17 @@ if __name__ == '__main__':
     # result = accountAPI.get_account_balance()
 
     # copy trading api
-    copyTradingAPI = CopyTrading.CopyTradingAPI(api_key, secret_key, passphrase, False, flag)
-    result = copyTradingAPI.first_copy_settings(name='127BE725D7F66EED', copymode='SMART_COPY', amount='100')
-    # result = copyTradingAPI.first_copy_settings1(name='127BE725D7F66EED', amount='100')
+    # 540D011FDACCB47A 百万
+    copyTradingAPI = CopyTrading.CopyTradingAPI(api_key, secret_key, passphrase, False, flag, debug=False)
+    # result = copyTradingAPI.first_copy_settings(name='540D011FDACCB47A', copymode='SMART_COPY', amount='100')
 
-    print(json.dumps(result))
-
+    while True:
+        result = copyTradingAPI.first_copy_settings1(name='540D011FDACCB47A', amount='100')
+        # print(json.dumps(result))
+        # print(result.get("code"))
+        if result.get("code") == '59206':
+            print("Code is not 1. Sleeping for 1 second.")
+            time.sleep(0.5)
+        else:
+            print("Code is 1. Ending loop.")
+            break
